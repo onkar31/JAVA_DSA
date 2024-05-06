ae1c4824d2f13cb8a173401cde118b70c865d477
Onkar31 <onkarbhogle08@gmail.com>
2024-05-06 07:26:12 +0530

    Binary Search

---
+++ ARRAYS/BinarySearch.java
@@ -0,0 +1,31 @@
+
+// Time complexity : O(log n)
+
+public class BinarySearch {
+    
+    private static int binarySearch(int numbers[], int key) {
+        int start = 0, end = numbers.length -1;
+        
+        while(start <= end){
+            int mid = (start + end) / 2;
+            
+            //comparison
+            if(numbers[mid] == key){
+                return mid;
+            }
+            if(numbers[mid] < key){ //right
+                start = mid + 1;
+            }else { //left
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {2, 4, 6, 8, 10, 12, 14, 16};
+        int key = 10;
+        int index = binarySearch(numbers, key);
+        System.out.println("key present at index : "+ index);
+    }
+}
