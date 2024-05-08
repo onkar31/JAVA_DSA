323570173bbc67ffe375a2af81e1a7cc4e98499b
Onkar31 <onkarbhogle08@gmail.com>
2024-05-08 07:32:34 +0530

    Pairs in an Array

---
+++ ARRAYS/PairsInArray.java
@@ -0,0 +1,22 @@
+
+
+public class PairsInArray{
+
+    public static int printPairs(int numbers[]){
+        int tp = 0;
+        for(int i = 0; i < numbers.length ; i++){
+           for(int j = i + 1 ; j < numbers.length ; j++){
+            System.out.print("("+ numbers[i] + "," + numbers[j] + ")");
+            tp++;
+           }
+           System.out.println();
+        }
+        return tp;
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {2, 4, 6, 8, 10};
+        int totalPairs = printPairs(numbers);
+        System.out.println("Total Pairs : "+ totalPairs);
+    }
+}
